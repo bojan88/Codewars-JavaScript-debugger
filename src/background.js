ed13223c7f516e32b692b86b79a79203c9d4de5a
bojan <bojan88@gmail.com>
2017-03-05 17:24:39 +0100

    Changed the way we are checking if it's codewars kata page - #1

--- src/background.js
+++ src/background.js
@@ -1,28 +1,31 @@
 var tabId;
 
-chrome.runtime.onConnect.addListener(function (port) {
-  if (port.name === "devtools-page" &&
-      port.sender.tab &&
-      port.sender.tab.title.match(/Developer Tools.*https?:\/\/(www.)?codewars.com\/kata\/.+\/javascript/)) {
+chrome.runtime.onConnect.addListener(function(port) {
+  if (port.name !== "devtools-page" || port.sender.id !== chrome.runtime.id) {
+    return;
+  }
 
-    var messageHanler = function(message, sender) {
-      if (message.action === "init") {
-        tabId = message.tabId;
+  chrome.tabs.query({active: true}, function(tabs) {
+    if(tabs && tabs.length > 0 && tabs[0].url.match(/https?:\/\/(www.)?codewars.com\/kata\/.+\/javascript/)) {
+      var messageHanler = function(message, sender) {
+        if (message.action === "init") {
+          tabId = message.tabId;
+          chrome.tabs.sendMessage(tabId, {
+            action: 'init',
+            source: 'cw-dev-tools-msg'
+          });
+        }
+      };
+
+      port.onMessage.addListener(messageHanler);
+
+      port.onDisconnect.addListener(function(port) {
+        port.onMessage.removeListener(messageHanler);
         chrome.tabs.sendMessage(tabId, {
-          action: 'init',
+          action: 'clean',
           source: 'cw-dev-tools-msg'
         });
-      }
-    };
-
-    port.onMessage.addListener(messageHanler);
-
-    port.onDisconnect.addListener(function(port) {
-      port.onMessage.removeListener(messageHanler);
-      chrome.tabs.sendMessage(tabId, {
-        action: 'clean',
-        source: 'cw-dev-tools-msg'
       });
-    });
-  }
+    }
+  });
 });
